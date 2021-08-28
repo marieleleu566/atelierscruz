0fa960ec41058d0f99b0073ff2521bbfbb6049fd
Marie Leleu <marieleleu@icloud.com>
2021-08-28 13:36:07 +0200

    apropos page done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'apropos', to: 'pages#apropos'
   get 'broderie', to: 'pages#broderie'
-  get 'tapisserie', to: 'pages#broderie'
+  get 'tapisserie', to: 'pages#tapisserie'
   get 'contact', to: 'pages#contact'
 end
