2519146c1af83509170069592ce59e2eb27f7d8b
Marie Leleu <marieleleu@icloud.com>
2021-09-22 14:56:01 +0200

    homepage done

--- app/javascript/components/nav.js
+++ app/javascript/components/nav.js
@@ -1,42 +1,10 @@
 const nav = () => {
-  $(function() {
-
-      var newHash      = "",
-          $mainContent = $("#main-content"),
-          $pageWrap    = $("#page-wrap"),
-          baseHeight   = 1,
-          $el;
-
-      $pageWrap.height($pageWrap.height());
-      baseHeight = $pageWrap.height() - $mainContent.height();
-
-      $("nav").delegate("a", "click", function() {
-          window.location.hash = $(this).attr("href");
-          return false;
-      });
-
-      $(window).bind('hashchange', function(){
-
-          newHash = window.location.hash.substring(1);
-
-          if (newHash) {
-              $mainContent
-                  .find("#guts")
-                  .fadeOut(200, function() {
-                      $mainContent.hide().load(newHash + " #guts", function() {
-                          $mainContent.fadeIn(200, function() {
-                          });
-                          $("nav a").removeClass("current");
-                          $("nav a[href*=\\"+newHash+"]").addClass("current");
-                      });
-                  });
-          };
-
-      });
-
-      $(window).trigger('hashchange');
-
-  });
+var burgerMenu = document.getElementById('burger-menu');
+var overlay = document.getElementById('menu');
+burgerMenu.addEventListener('click',function(){
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
 }
 
 export {nav};
